7ad36864b28e08d99dc577989d22f5e9ec514c30
Simon Lovelock <simon@simons-mbp.home>
2019-01-07 21:13:05 +0000

    layout completed

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Simon Lovelock on 07/01/2019.
+//  Copyright © 2019 Simon Lovelock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
