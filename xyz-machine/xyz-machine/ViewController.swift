495f17d22d7887d0e92ecf092f33b5d1b0009f6d
Simon Lovelock <simon@simons-mbp.home>
2019-01-09 21:03:41 +0000

    completed xyz app

--- xyz-machine/xyz-machine/ViewController.swift
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+        
     }
 
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 5
+        formatter.minimumFractionDigits = 2
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y = formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        
+        xLabel.text = "X: \(x)"
+        yLabel.text = "Y: \(y)"
+        zLabel.text = "Z: \(z)"
+        
+        print(x, y, z)
+    
+    }
 
 }
 
